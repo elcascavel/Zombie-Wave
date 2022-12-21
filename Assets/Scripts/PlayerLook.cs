199bb399fcd19c3549113a24dd6fb0c34c58bfae
Cascavel <simao.ramaral@gmail.com>
2022-12-21 22:02:34 +0000

    Weapon follows camera

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     public Camera cam;
+    [SerializeField] private GameObject playerHands;
     private float xRotation = 0f;
     public float xSensitivity = 30f;
     public float ySensitivity = 30f;
@@ -17,7 +18,8 @@ public class PlayerLook : MonoBehaviour
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        cam.transform.eulerAngles = new Vector3(xRotation, cam.transform.eulerAngles.y, 0f);
+        playerHands.transform.eulerAngles = new Vector3(xRotation, cam.transform.eulerAngles.y, 0f);
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
     }
 }
